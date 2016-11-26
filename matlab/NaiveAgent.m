15f693575a90e1fc8701b89dc0676ca436a3cf7d
kunalmenda <kunal.menda@gmail.com>
2016-11-25 17:52:13 -0800

    started building environment

--- matlab/NaiveAgent.m
+++ matlab/NaiveAgent.m
@@ -1,32 +1,44 @@
-classdef NaiveAgent < Handle
+classdef NaiveAgent < handle
     % NAIVEAGENT Plays a naive strategy when called
     % 
     
     properties
         hand = -1;
         total_coins = -1;
-        tot_head_dist = [];
+        num_players = -1;
+        best_bet = -1;
         thresh = 0.5;
     end
     
     methods
-        function obj = NaiveAgent(hand, total_coins, thresh)
+        function obj = NaiveAgent(hand, total_coins, num_players, thresh)
             % Inputs: hand = num heads in this agent's hand
             %         total_coins = total coins in game
+            %         num_players = number of players in the game
             %         thresh = probability against which agent
             %           calls a bet or chooses a bet
             obj.hand = hand;
             obj.total_coins = total_coins;
-            obj.best_bet = hand;
-            for bet = hand:total_coins
-                prob = binocdf(bet-hand-1, total_coins - hand, 0.5);
-                if prob <= thresh 
-                    obj.best_bet = hand;
-                else
-                    break
-                end
-            end
+            obj.num_players = num_players;
             obj.thresh = thresh;
+%             obj.best_bet = hand;
+%             for bet = hand:total_coins
+%                 prob = binocdf(bet-hand-1, total_coins - hand, 0.5);
+%                 if prob <= thresh 
+%                     obj.best_bet = hand;
+%                 else
+%                     break
+%                 end
+%             end
+            
+            % compute coins held by other players
+            available_coins = round(total_coins*(1 - 1/num_players));
+            display(available_coins)
+            % compute distribution over probabilities of getting coins
+            bincdf = binocdf([0:available_coins],available_coins,0.5);
+            % set best bet to largest safe bet, add player's hand
+            obj.best_bet = hand + find(bincdf<=thresh,1,'last')-1;
+            
         end
         
         function next_bet = playStrategy(obj,curr_bet)
