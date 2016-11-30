83b1c6666dc792ad62ea4fd02145d70aed39203a
Apoorva Sharma <me@apoorvasharma.com>
2016-11-29 16:13:25 -0800

    tweaked basic test a bit

--- matlab/basic_test.m
+++ matlab/basic_test.m
@@ -3,17 +3,18 @@
 player1 = NaiveAgent(0.5);
 player2 = NaiveAgent(0.5);
 player3 = NaiveAgent(0.5);
+player4 = NaiveAgent(0.5);
 
 coins_per_player = 5;
 
-losses = [0,0,0];
+losses = [0,0,0,0];
 
 niter = 1000;
 for iter = 1:niter
-    env = Environment([player1 player2, player3], 5, true);
+    env = Environment([player1 player2 player3 player4], 5, true);
     loser = env.playGame();
     losses(loser) = losses(loser) + 1;
 end
 
-bar(losses);
-title('Number of losses of each player');
+bar(losses./sum(losses));
+title('Lose rate of each player');
