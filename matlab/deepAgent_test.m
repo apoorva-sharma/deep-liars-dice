fd79d8bc39e1254d5b461b57fa98d316e6a02209
kunalmenda <kunal.menda@gmail.com>
2016-12-03 22:48:07 -0800

    commented out observer initial training

--- matlab/deepAgent_test.m
+++ matlab/deepAgent_test.m
@@ -1,35 +1,35 @@
 %% First, train a basic observer model from watching naive agents play
 
-player1 = NaiveAgent(0.5);
-player2 = NaiveAgent(0.5);
-player3 = NaiveAgent(0.5);
-player4 = NaiveAgent(0.5);
-
-coins_per_player = 5;
-total_coins = coins_per_player*4;
-oneHotEye = eye(total_coins+1-coins_per_player);
-
-training_examples = [];
-training_labels = [];
-
-tic
-niter = 10000;
-for iter = 1:niter
-    env = Environment({player1 player2 player3 player4}, 5, true);
-    env.playGame();
-    X = env.X;
-    X(isnan(X)) = -10;
-    training_examples = [training_examples; X];
-    Y = env.Y;
-    oneHotY = oneHotEye(Y+1,:);
-    training_labels = [training_labels; oneHotY]; 
-end
-
-training_examples = training_examples';
-training_labels = training_labels';
-%%
-obsNet = initObserverNet();
-[obsNet,tr] = train(obsNet,training_examples, training_labels);
+% player1 = NaiveAgent(0.5);
+% player2 = NaiveAgent(0.5);
+% player3 = NaiveAgent(0.5);
+% player4 = NaiveAgent(0.5);
+% 
+% coins_per_player = 5;
+% total_coins = coins_per_player*4;
+% oneHotEye = eye(total_coins+1-coins_per_player);
+% 
+% training_examples = [];
+% training_labels = [];
+% 
+% tic
+% niter = 10000;
+% for iter = 1:niter
+%     env = Environment({player1 player2 player3 player4}, 5, true);
+%     env.playGame();
+%     X = env.X;
+%     X(isnan(X)) = -10;
+%     training_examples = [training_examples; X];
+%     Y = env.Y;
+%     oneHotY = oneHotEye(Y+1,:);
+%     training_labels = [training_labels; oneHotY]; 
+% end
+% 
+% training_examples = training_examples';
+% training_labels = training_labels';
+% %%
+% obsNet = initObserverNet();
+% [obsNet,tr] = train(obsNet,training_examples, training_labels);
 
 %% Load previously trained ObsNet
 load MKObsTest.mat
