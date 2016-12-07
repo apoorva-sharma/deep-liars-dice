b617beb9543db845139455e9f94d0de219a45af5
Apoorva Sharma <me@apoorvasharma.com>
2016-12-07 00:08:55 -0800

    create a learning curve during training

--- matlab/self_play_test.m
+++ matlab/self_play_test.m
@@ -30,36 +30,15 @@ player2 = DeepAgent(pObsNet, pPiNet, pQNet, obsXbuf, obsYbuf, PiXbuf, QXbuf);
 player3 = DeepAgent(pObsNet, pPiNet, pQNet, obsXbuf, obsYbuf, PiXbuf, QXbuf);
 player4 = DeepAgent(pObsNet, pPiNet, pQNet, obsXbuf, obsYbuf, PiXbuf, QXbuf);
 
-%% Play against each other, training all nets
-player1.training = true;
-player2.training = true;
-player3.training = true;
-player4.training = true;
-playerlist = {player1 player2 player3 player4};
-
-tic
-
-niter = 50000;
-h = waitbar(0,'Please wait...');
-for iter = 1:niter
-    waitbar(iter/niter);
-    ordering = randperm(4);
-    env = Environment(playerlist(ordering), coins_per_player, true);
-    env.playGame();
-end
-close(h)
-toc
-
-%% Play to WIN against Naive Agents
+%% test untrained deep agents against NaiveAgents
 player1.training = false;
-
 naive1 = NaiveAgent(0.5);
 naive2 = NaiveAgent(0.5);
 naive3 = NaiveAgent(0.5);
 
 playerlist = {player1 naive1 naive2 naive3};
 losses = [0,0,0,0];
-niter = 5000;
+niter = 10000;
 h = waitbar(0,'Please wait...');
 for iter = 1:niter
     waitbar(iter/niter);
@@ -70,8 +49,79 @@ for iter = 1:niter
 end
 close(h)
 
+untrainedlosses = losses(1);
+untrainedwins = sum(losses(2:end));
+
+% The above section causes bugs in the remaining sections. Run without this
+% section after running this section once.
+
+%% run training and testing to plot a learning curve of sorts
+player1.training = true;
+nouteriter = 16;
+ntrainiter = 3000;
+
+totallosses = zeros(nouteriter,1);
+totalwins = zeros(nouteriter,1);
+
+for outeriter = 1:nouteriter
+    %% Play against each other, training all nets
+    player1.training = true;
+    player2.training = true;
+    player3.training = true;
+    player4.training = true;
+    playerlist = {player1 player2 player3 player4};
+
+    tic
+
+    h = waitbar(0,strcat('Iteration ',num2str(outeriter)));
+    for iter = 1:ntrainiter
+        waitbar(iter/ntrainiter);
+        ordering = randperm(4);
+        env = Environment(playerlist(ordering), coins_per_player, true);
+        env.playGame();
+    end
+    close(h)
+    toc
+
+    %% Play to WIN against Naive Agents
+    player1.training = false;
+
+    naive1 = NaiveAgent(0.5);
+    naive2 = NaiveAgent(0.5);
+    naive3 = NaiveAgent(0.5);
+
+    playerlist = {player1 naive1 naive2 naive3};
+    losses = [0,0,0,0];
+    niter = 10000;
+    h = waitbar(0,'Please wait...');
+    for iter = 1:niter
+        waitbar(iter/niter);
+        ordering = randperm(4);
+        env = Environment(playerlist(ordering), coins_per_player, true);
+        loser = env.playGame();
+        losses(ordering(loser)) = losses(ordering(loser)) + 1;
+    end
+    close(h)
+    
+    totallosses(outeriter) = losses(1);
+    totalwins(outeriter) = sum(losses(2:end));
+end
+
+totallosses = [untrainedlosses; totallosses];
+totalwins = [untrainedwins; totalwins];
 %%
-figure()
+num_train_iters = [0:nouteriter]*ntrainiter;
+
+loss_rate = totallosses./(totallosses + totalwins);
+figure(1)
+plot(num_train_iters, loss_rate)
+xlabel('Number of Self Play Games Played');
+ylabel('Average loss rate against 3 Naive Agents');
+title('Training Curve');
+grid on;
+
+%%
+figure(2)
 pvals = linspace(0,0.5,500);
 alpha = losses;
 beta = sum(losses) - losses;
